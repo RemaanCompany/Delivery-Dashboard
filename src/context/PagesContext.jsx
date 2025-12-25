ade2530e14e73e8ef0eee6f34c80718c230caf18
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-12-25 14:55:47 +0300

    add icons to pages context

--- src/context/PagesContext.jsx
+++ src/context/PagesContext.jsx
@@ -4,12 +4,32 @@ export const pagesContext = createContext();
 
 export default function PagesContext({ children }) {
   const [pagesState, setPagesState] = useState({
-    home: "Home",
-    users: "Users",
-    drivers: "Drivers",
-    locations: "Locations",
-    orders: "Orders",
-    finance: "Finance",
+    pages: [
+      {
+        home: "Home",
+        icon: <i className="fa-solid fa-house"></i>,
+      },
+      {
+        users: "Users",
+        icon: <i className="fa-solid fa-users"></i>,
+      },
+      {
+        drivers: "Drivers",
+        icon: <i className="fa-solid fa-motorcycle"></i>,
+      },
+      {
+        locations: "Locations",
+        icon: <i className="fa-solid fa-location-dot"></i>,
+      },
+      {
+        orders: "Orders",
+        icon: <i className="fa-solid fa-box-archive"></i>,
+      },
+      {
+        finance: "Finance",
+        icon: <i className="fa-solid fa-wallet"></i>,
+      },
+    ],
     selected: "Home",
   });
 
