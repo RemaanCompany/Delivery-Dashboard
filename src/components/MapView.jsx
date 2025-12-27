c9ca6dab1ecfb064d11a1cf09cdbfa88afd2919c
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-12-27 14:56:28 +0300

    add loading status to mapView component

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,11 +1,24 @@
-import React from "react";
 import Map from "react-map-gl/mapbox";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { useState } from "react";
 
-const MapView = () => {
+export default function MapView() {
+  const [isMapLoading, setIsMapLoading] = useState(true);
+
   return (
     <div className="rounded-[10px] bg-white flex-3">
-      <div className="w-full h-[355px] rounded-[5px] overflow-hidden mt-2">
+      <div className="w-full h-89 rounded-[5px] overflow-hidden mt-2 relative">
+        {isMapLoading && (
+          <div className="absolute inset-0 z-10 flex items-center justify-center bg-white/80 backdrop-blur-sm">
+            <div className="flex flex-col items-center gap-4">
+              <div className="w-16 h-16 border-[6px] border-gray-300 border-t-black rounded-full animate-spin"></div>
+              <span className="text-lg font-semibold text-gray-700">
+                Loading map...
+              </span>
+            </div>
+          </div>
+        )}
+
         <Map
           mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
           initialViewState={{
@@ -14,10 +27,9 @@ const MapView = () => {
             zoom: 10,
           }}
           mapStyle="mapbox://styles/mapbox/streets-v12"
+          onLoad={() => setIsMapLoading(false)}
         />
       </div>
     </div>
   );
-};
-
-export default MapView;
+}
